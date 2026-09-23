b89eaf57b23e899e83449451b9036a9e7be01f01
tk0520 <tk0520@users.noreply.github.com>
2026-09-24 04:56:04 +0900

    chore: 변수명 정리

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import render_template 
 from flask import Flask
 
-server = Flask(__name__)
+app = Flask(__name__)
 
-@server.route("/admin")
-def admin_panel():
+@app.route("/admin")
+def get_admin_panel():
     return render_template("admin.html")
 
-@server.route("/result")
+@app.route("/result")
 def get_result():
     return render_template("result.html")
